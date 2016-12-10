93f0c2cde6a6122c75499eb4736e85cf096e002b
Alan Brown <alan@mailmob.co.uk>
2016-12-10 22:59:26 +0000

    changed pwm settings

--- servo.py
+++ servo.py
@@ -16,53 +16,13 @@ p = GPIO.PWM(4, 50)    # create an object p for PWM on port 25 at 50 Hertz
                         # different names for each port   
                         # e.g. p1, p2, motor, servo1 etc.  
 print("Starting PWM")  
-p.start(5)             # start the PWM on 50 percent duty cycle  
+p.start(2)             # start the PWM on 50 percent duty cycle  
                         # duty cycle value can be 0.0 to 100.0%, floats are OK  
 
 time.sleep(1)  
-p.ChangeDutyCycle(6)  
-
-time.sleep(1)  
-p.ChangeDutyCycle(7)  
-
-time.sleep(1)  
-p.ChangeDutyCycle(8)  
-
-time.sleep(1)  
-p.ChangeDutyCycle(9)  
-
-time.sleep(1)  
-p.ChangeDutyCycle(10)  
-
-time.sleep(1)  
-p.ChangeDutyCycle(11)  
-
-time.sleep(5)  
-p.ChangeDutyCycle(10)  
-
-time.sleep(1)  
-p.ChangeDutyCycle(9)  
-
-time.sleep(1)  
-p.ChangeDutyCycle(8)  
-
-time.sleep(1)  
-p.ChangeDutyCycle(7)  
-
-time.sleep(1)  
-p.ChangeDutyCycle(6)  
-
-time.sleep(1)  
-p.ChangeDutyCycle(5)  
-
-time.sleep(1)  
-p.ChangeDutyCycle(4)  
-
-time.sleep(1)  
-p.ChangeDutyCycle(3)  
-
-time.sleep(1)  
-p.ChangeDutyCycle(2)   
+p.ChangeDutyCycle(6)
+time.sleep(0.05)  
+p.ChangeDutyCycle(2)  
 
 time.sleep(2)  
 print("Stopping the script")
