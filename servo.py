ef29a9b7b07ad5ddc5e6eb14f2045114472e6bad
Alan Brown <alan@mailmob.co.uk>
2016-12-10 22:28:33 +0000

    initial commit

--- servo.py
+++ servo.py
@@ -9,20 +9,22 @@ import time
 
 GPIO.setmode(GPIO.BCM)  # choose BCM or BOARD numbering schemes. I use BCM  
   
-GPIO.setup(25, GPIO.OUT)# set GPIO 25 as an output. You can use any GPIO port  
+GPIO.setup(4, GPIO.OUT)# set GPIO 25 as an output. You can use any GPIO port  
   
 p = GPIO.PWM(25, 50)    # create an object p for PWM on port 25 at 50 Hertz  
                         # you can have more than one of these, but they need  
                         # different names for each port   
                         # e.g. p1, p2, motor, servo1 etc.  
-  
+print("Starting PWM")  
 p.start(50)             # start the PWM on 50 percent duty cycle  
                         # duty cycle value can be 0.0 to 100.0%, floats are OK  
 
 time.sleep(2)  
+print("Setting Duty Cycle to 90%")
 p.ChangeDutyCycle(90)   # change the duty cycle to 90%  
 
 time.sleep(2)  
+print("Setting Frequency to 100")
 p.ChangeFrequency(100)  # change the frequency to 100 Hz (floats also work)  
                         # e.g. 100.5, 5.2  
 time.sleep(2)  
